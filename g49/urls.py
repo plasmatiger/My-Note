4a9a828353b0987e4d5b7a7940cad2db21eaaa6d
Akshat Anand <aakshat84@gmail.com>
2017-08-15 12:18:22 +0530

    The system files of django project

---
+++ g49/urls.py
@@ -0,0 +1,28 @@
+"""g49 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.8/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Add an import:  from blog import urls as blog_urls
+    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from login_logout.views import homepage
+admin.autodiscover()
+
+urlpatterns = [
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login_logout/', include('login_logout.urls', namespace="login_logout")),
+    url(r'^notes/', include('notes.urls',namespace="notes")),
+    url(r'^$', homepage,name='homepage'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
